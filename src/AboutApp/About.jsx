74ca7b600700784dbc00d8e7205bdbeea24ec246
abahanointed@gmail.com <abahanointed@gmail.com>
2023-08-23 10:23:46 +0100

    new

--- src/AboutApp/About.jsx
+++ src/AboutApp/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
      <h1>Threads Video Downloader</h1>
     <p>GetThreadsVideos.com is an online tool that helps you download Threads videos for free in a few seconds. 
         You can now save Threads videos and still retain high quality in MP4 and HD resolution. 
-        Our Instagram Threads video downloader is secure, efficient and seamless with no extra installations. You only need a Threads video link to paste and download without hassle!</p>
+        Our Instagram Threads video downloader is secure, efficient and seamless with no extra installations. You only need a Threads video link to paste and download without any hassle!</p>
     </div>
   )
 }
