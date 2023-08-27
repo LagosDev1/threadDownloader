ed50d66adf708ce79615f54783c67062d4801dde
abahanointed@gmail.com <abahanointed@gmail.com>
2023-08-27 20:35:37 +0100

    Change

--- src/FAQ/Faq.jsx
+++ src/FAQ/Faq.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Data } from './Data';
-import styled from 'styled-components'
-import {IconContext} from 'react-icons'
-import {FiPlus, FiMinus} from 'react-icons/fi'
+// import { Data } from './Data';
+// import styled from 'styled-components'
+// import {IconContext} from 'react-icons'
+// import {FiPlus, FiMinus} from 'react-icons/fi'
 
 
 const Faq = () => {
