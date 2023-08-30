33e3967b0c2cf8808f3a9a175a088aaf775af2d7
abahanointed@gmail.com <abahanointed@gmail.com>
2023-08-30 15:29:10 +0100

    Changes

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer">
       <div class="logo"><span>Threads</span>Mate</div>
-      <li href="#contact">Contact Us</li>
+      <a href="#contact">Contact Us</a>
       <a href="#nav">Faq</a>
     </div>
   )
