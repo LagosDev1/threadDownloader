cf40fa67ab8c427f47c45e0df26cf5ddc38233d7
abahanointed@gmail.com <abahanointed@gmail.com>
2023-09-05 22:21:49 +0100

    show answer

--- src/FAQ/Faq.jsx
+++ src/FAQ/Faq.jsx
@@ -4,13 +4,19 @@ import React from 'react';
 // import {IconContext} from 'react-icons'
 // import {FiPlus, FiMinus} from 'react-icons/fi'
 
+const showAnswer = () => {
+  let answer = document.getElementById('answer');
+  if (answer)  return;
+  let div =document.createElement("div");
+  let text= "This is the Answer";
+}
 
 const Faq = () => {
   return (
     <div className="faq" id='faq'>
         <h1 className="faqHeader" id="faqHeader">Faq</h1>
         <article className='question'>Is GetThreadsVideos.com free to use</article>
-        <p className="answer" id="answer">Yes, the Threads Video Downloader tool at GetThreadsVideos.com is completely free to use. We believe in providing a convenient and accessible solution for downloading Threads videos without any cost.</p>
+        <p className="answer" id="answer" onClick={showAnswer}>Yes, the Threads Video Downloader tool at GetThreadsVideos.com is completely free to use. We believe in providing a convenient and accessible solution for downloading Threads videos without any cost.</p>
     </div>
   )
 }
