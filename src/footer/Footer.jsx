57a5d1fdb20d69517ab11881e425d6c32c4518fc
abahanointed@gmail.com <abahanointed@gmail.com>
2023-09-06 10:45:54 +0100

    Foooter

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -4,9 +4,9 @@ import './footer.css';
 function Footer() {
   return (
     <div className="footer">
-      <div class="logo"><span>Threads</span>Mate</div>
+      {/* <div class="logo"><span>Threads</span>Mate</div>
       <a href="#contact">Contact Us</a>
-      <a href="#nav">Faq</a>
+      <a href="#nav">Faq</a> */}
     </div>
   )
 }
